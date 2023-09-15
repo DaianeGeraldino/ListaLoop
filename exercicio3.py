0a28bb56c8b1bec198356dea257cb6f709010801
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2023-09-14 21:27:13 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,6 +1,15 @@
-from random import shuffle
+lista_compras = []
+cont = True
 
-lista = ['abacaxi', 'banana', 'maça', 'manga']
+while cont:
+    usuario = int(input("Escreva o que deseja colocar na lista de compras "))
+    lista_numeros.append(usuario)
 
-shuffle(lista)
-print(lista)
+    condicao = int(input("Digite 1 para parar e 2 para continuar "))
+    if condicao == 1:
+        cont = False
+    elif condicao == 2:
+        cont = True
+
+lista_numeros.reverse()
+print(lista_numeros)
